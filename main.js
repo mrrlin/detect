b26eb11cecf262ef76d0f8e32bfb90f11e22ef60
Angelina <bunny.iblis@gmail.com>
2023-05-28 22:59:13 +0300

    Add changes

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 import { loadGLTF } from "./libs/loader.js";
+
+const buttonStart = document.getElementById('startButton');
+const buttonStop = document.getElementById('stopButton');
+buttonStop.style.display = 'none';
+
 const THREE = window.MINDAR.IMAGE.THREE;
 
 const mindarThree = new window.MINDAR.IMAGE.MindARThree({
   container: document.body,
-  imageTargetSrc: './assets/targets/furn-target.mind'
+  imageTargetSrc: './assets/targets/furn-target.mind',
+  uiScanning: '#scanning',
+  uiLoading: 'no'
 });
 
 function startMindAR() {
@@ -17,33 +24,36 @@ function startMindAR() {
     furnModel.scene.scale.set(0.1, 0.1, 0.1);
     furnModel.scene.position.set(0, -0.4, 0);
 
-    const bear = await loadGLTF('./assets/models/chair/scene.gltf');
-    bear.scene.scale.set(0.1, 0.1, 0.1);
-    bear.scene.position.set(0, -0.4, 0);
-
     const furnModelAnchor = mindarThree.addAnchor(0);
     furnModelAnchor.group.add(furnModel.scene);
-
-    const bearAnchor = mindarThree.addAnchor(1);
-    bearAnchor.group.add(bear.scene);
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
       renderer.render(scene, camera);
+    });
+
+    const rangeInput = document.getElementById('sizeRange');
+    rangeInput.addEventListener('input', () => {
+      const scaleValue = rangeInput.value;
+      furnModel.scene.scale.set(scaleValue, scaleValue, scaleValue);
     });
   }
   start();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('startButton');
-  button.addEventListener('click', startMindAR);
+  buttonStart.addEventListener('click', () => {
+    startMindAR();
+    buttonStart.style.display = 'none';
+    buttonStop.style.display = 'block';
+  });
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('stopButton');
-  button.addEventListener('click', () => {
+  buttonStop.addEventListener('click', () => {
     mindarThree.stop();
     console.log('click');
+    buttonStop.style.display = 'none';
+    buttonStart.style.display = 'block';
   });
 });
